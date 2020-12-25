259551ea7144ff0ff08a5a3eed1fdce3a9641faa
andreypetrischin <50768770+andreypetrischin@users.noreply.github.com>
2020-12-25 21:30:33 +0100

    Shoot Right

---
+++ Assets/Scripts/Mechanics/MoveTrail.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTrail : MonoBehaviour
+{
+
+    public int moveSpeed;
+   
+
+  
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+     
+
+
+    }
+}
---
+++ Assets/Scripts/Mechanics/MoveTrailUp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTrailUp : MonoBehaviour
+{
+    public int moveSpeed;
+
+
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+    }
+}
