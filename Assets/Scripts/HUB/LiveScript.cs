393ebf0c99d7ba65340a4aff70f7c3d981b8fb93
andreypetrischin <50768770+andreypetrischin@users.noreply.github.com>
2021-01-27 03:22:27 +0100

    HUB

---
+++ Assets/Scripts/HUB/LiveScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LiveScript : MonoBehaviour
+{
+    public GameObject parent;
+    GameObject backgroundAudio;
+
+
+    private void Awake()
+    {
+        backgroundAudio = GameObject.Find("BackgroundAudio");
+    }
+    public static int liveValue = 4;
+    Text Live;
+    void Start()
+    {
+        Live = GetComponent<Text>();
+    }
+    void Update()
+    {
+        Live.text = " " + liveValue;
+
+        if(liveValue < 0)
+        {
+            backgroundAudio.SetActive(false);
+            SaundManagerScript.PlaySound("GameOver");
+            parent.transform.GetChild(0).gameObject.SetActive(true);
+            AudioListener.volume = 0;
+        }
+    }
+}
