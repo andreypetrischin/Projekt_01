55738cee110b49f6fb3abbeb45e2fd56b53f4161
andreypetrischin <50768770+andreypetrischin@users.noreply.github.com>
2021-01-27 17:47:37 +0100

    Jump Count

---
+++ Assets/JumpCount.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpCount : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ScoreScript.scoreValue += 100;
+        }
+    }
+  
+
+}
